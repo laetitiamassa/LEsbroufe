9ddaf1b23ee34a873ea083f036081d2707f8b134
laetitia massa <laetitiamassa@gmail.com>
2014-01-19 16:42:04 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lesbroufe::Application.config.secret_key_base = 'a295912466d0eab19463a3cfb61aca151ee31d69608925f2699fb4f5078ba2f5d289c3085e8e99235ff72cf0b10d738b51b1c97711be15a6800c1e383800949b'
